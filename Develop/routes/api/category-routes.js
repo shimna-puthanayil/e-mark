2ccbb8ed87c768473371745ad84bc43a9d7606f2
shimna <chimmu.p@gmail.com>
2023-08-15 21:53:50 +1000

    added POST route to add category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,8 +4,7 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // find all categories including its associated Products
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
@@ -17,8 +16,7 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // find one category by its `id` value including its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
@@ -35,8 +33,14 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
