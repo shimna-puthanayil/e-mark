f8f25e40faa83166619c33943a2dcbd5a186ed89
shimna <chimmu.p@gmail.com>
2023-08-19 00:13:20 +1000

    added comments

--- models/Tag.js
+++ models/Tag.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
-
+// fields and rules for Tag model
 Tag.init(
   {
     id: {
